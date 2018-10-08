571147fd64d65caef767e7a91f8c73451401a61d
Kevin <kphyde@gmail.com>
2018-10-08 13:59:36 -0700

    complete search and query strings, fix broken ajax function, provide random number function, retrieve list of videos

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,37 +1,44 @@
+$(function () {
 
-
-// $("#submit").on("submit", function (e) {
-//   alert("submitted");
-//   e.preventDefault();
-
-//   var level = $("#skill_level").val();
-//   var type = $("#lesson_format").val();
-//   debugger;
-//   var length;
-//   var genre;
-//   var keywords;
-
-// })
-
-$(function(){
-
-  alert("js loaded");
+  var randomSelect = function (length) {
+    return Math.floor((Math.random() * length) + 1)
+  }
 
   $(".toggle-sidebar").on("click", function () {
     $("#sidebar").toggleClass("collapsed");
-    $("#content").toggleClass("col-md-12 col-md-9");  
+    $("#content").toggleClass("col-md-12 col-md-9");
   })
-  
-  $( "#new_search" ).submit(function( event ) {
-    alert( "Handler for .submit() called." );
+
+  $("#new_search").submit(function (event) {
     event.preventDefault();
     var level = $("#skill_level").val();
     var type = $("#lesson_format").val();
     var length = $("#lesson_length").val();
     var genre = $("#genre").val();
     var keywords = $("#keyword").val();
-    debugger;
-  });
+
+
+    // BUILD A QUERY STRING
+    var searchString = level + "+" + genre + "+" + "guitar" + "+" + type + "+" + keywords; // IS LENGTH A SPECIAL STRING?
+    var apiKey = "AIzaSyDZ0DP1vMYabAwTtRsbTfRAqnswG0jTh_M";
+    var queryString = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=relevance&q=" + searchString + "&topicId=%2Fm%2F04rlf&type=video&videoCaption=any&key=" + apiKey;
+
+    // API CALL TO YOUTUBE HERE
+
     
-  
+
+    $.ajax({
+      type: "get",
+      url: queryString,
+      success: function (response) {
+        console.log(response);
+        var videoList = response.items;
+        console.log(videoList);
+        var selection = randomSelect(videoList.length);
+        debugger;
+      }
+    });
   });
+
+
+});
