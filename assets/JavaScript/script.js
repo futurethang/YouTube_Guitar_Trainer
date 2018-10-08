bd50d5dc48f8876548be21a540c989cccb36a7ed
Kevin <kphyde@gmail.com>
2018-10-08 11:04:27 -0700

    submit event assigns variables to input values

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,4 +1,37 @@
-$(".toggle-sidebar").on("click", function () {
-  $("#sidebar").toggleClass("collapsed");
-  $("#content").toggleClass("col-md-12 col-md-9");  
-})
+
+
+// $("#submit").on("submit", function (e) {
+//   alert("submitted");
+//   e.preventDefault();
+
+//   var level = $("#skill_level").val();
+//   var type = $("#lesson_format").val();
+//   debugger;
+//   var length;
+//   var genre;
+//   var keywords;
+
+// })
+
+$(function(){
+
+  alert("js loaded");
+
+  $(".toggle-sidebar").on("click", function () {
+    $("#sidebar").toggleClass("collapsed");
+    $("#content").toggleClass("col-md-12 col-md-9");  
+  })
+  
+  $( "#new_search" ).submit(function( event ) {
+    alert( "Handler for .submit() called." );
+    event.preventDefault();
+    var level = $("#skill_level").val();
+    var type = $("#lesson_format").val();
+    var length = $("#lesson_length").val();
+    var genre = $("#genre").val();
+    var keywords = $("#keyword").val();
+    debugger;
+  });
+    
+  
+  });
